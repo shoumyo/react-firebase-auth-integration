c9c197df86b0ce75185d98d0139220e56ab538ad
Shoumyo <shoumyosrijito@gmail.com>
2026-06-20 12:44:04 +0600

    feat: implement sign-in functionality in Login component and update AuthProvider to include signUser method

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../Context/Auth_Context/Auth_Context';
 
 const Login = () => {
+  const {signUser}=use(AuthContext);
+
+  const handleSignIn=(event)=>{
+    event.preventDefault();
+    const email=event.target.email.value;
+    const password=event.target.password.value;
+    signUser(email,password)
+    .then()
+    .catch(error=>{
+      alert(error);
+    })
+
+  }
     return (
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,12 +28,12 @@ const Login = () => {
           </div>
           <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
             <div className="card-body">
-              <form>
+              <form onSubmit={handleSignIn}>
                 <fieldset className="fieldset">
                   <label className="label">Email</label>
-                  <input type="email" className="input" placeholder="Email" />
+                  <input type="email" name='email' className="input" placeholder="Email" />
                   <label className="label">Password</label>
-                  <input type="password" className="input" placeholder="Password" />
+                  <input type="password" name='password' className="input" placeholder="Password" />
                   <div>
                     <a className="link link-hover">Forgot password?</a>
                   </div>
--- src/Context/AuthProvider/AuthProvider.jsx
+++ src/Context/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthContext } from '../Auth_Context/Auth_Context';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../Firebase/Firebase.init';
 
 const AuthProvider = ({ children }) => {
@@ -9,9 +9,24 @@ const AuthProvider = ({ children }) => {
     .then()
     .catch();
   };
+  const signUser=(email,password)=>{
+    return signInWithEmailAndPassword(auth,email,password)
+    .then()
+    .catch()
+  }
+
+  onAuthStateChanged(auth,(currentUser)=>{
+    if(currentUser){
+        console.log("")
+    }
+    else{
+        console.log("")
+    }
+  })
 
   const authInfo = {
     createUser,
+    signUser,
   };
   return (
     <div>
