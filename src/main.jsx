aca6e471e55d059971b74e1189822608fe2afa92
Shoumyo <shoumyosrijito@gmail.com>
2026-06-20 16:21:54 +0600

    feat: add Profile component and integrate with routing; implement private route for Orders

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
 import Root from './Layout/Root';
@@ -9,29 +9,43 @@ import Register from './Components/Register/Register';
 import Login from './Components/Login/Login';
 import AuthProvider from './Context/AuthProvider/AuthProvider';
 import Orders from './Components/Orders/Orders';
+import PrivateRoutes from './Routes/PrivateRoutes';
+import Profile from './Components/Profile/Profile';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    Component:Root,
-    children:[
+    Component: Root,
+    children: [
       {
-        index:true,
-        Component:Home
+        index: true,
+        Component: Home,
       },
       {
-        path:'/register',
-        Component:Register
+        path: '/register',
+        Component: Register,
       },
       {
-        path:'/login',
-        Component:Login
+        path: '/login',
+        Component: Login,
       },
       {
-        path:'/orders',
-        Component:Orders
-      }
-    ]
+        path: '/orders',
+        element: (
+          <PrivateRoutes>
+            <Orders></Orders>
+          </PrivateRoutes>
+        ),
+      },
+      {
+        path: '/profile',
+        element: (
+          <PrivateRoutes>
+            <Profile></Profile>
+          </PrivateRoutes>
+        ),
+      },
+    ],
   },
 ]);
 
